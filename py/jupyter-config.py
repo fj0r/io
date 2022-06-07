c4a45afc4c4b3c21620e8e19f8f9c6cf921d7882
nash <nash@iffy.me>
2022-06-07 14:35:49 +0800

    nu

--- py/jupyter-config.py
+++ py/jupyter-config.py
@@ -8,7 +8,7 @@ if os.getenv("JUPYTER_ROOT"):
     c.ServerApp.root_dir = os.getenv("JUPYTER_ROOT")
 
 c.ServerApp.allow_password_change = False
-c.ServerApp.terminado_settings = { "shell_command": ["/bin/zsh"] }
+c.ServerApp.terminado_settings = { "shell_command": ["/usr/local/bin/nu"] }
 c.ServerApp.allow_root = True
 c.ServerApp.ip = "0.0.0.0"
 c.ExtensionApp.open_browser = False
