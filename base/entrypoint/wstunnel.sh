1eb1cf35914d0ddee4786f7e36a4c352b8efdd0b
nash <nash@iffy.me>
2024-07-02 14:10:26 +0800

    entrypoint/wstunnel.sh

---
+++ base/entrypoint/wstunnel.sh
@@ -0,0 +1,5 @@
+if [ -n "$WSTUNNEL_PORT" ] || [ -n "$WSTUNNEL_PREFIX" ]; then
+    wstunnel server -r $WSTUNNEL_PREFIX ws://[::]:${WSTUNNEL_PORT:-9090} 2>&1 &
+
+    echo -n "$! " >> /var/run/services
+fi
